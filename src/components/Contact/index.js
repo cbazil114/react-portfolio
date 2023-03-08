e9482fc15c20c2081781c01c9db29a220e170415
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-03-08 17:41:39 -0500

    Changes to contact page-functionality not working with email and return responses

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// Here we import a helper function that will check if the email is valid
+import { checkPassword, validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
-    return (
-      <div class="container">
-      <div class= "card-contents"></div>
-        <h1>Contact Page</h1>
-        <form className="contact-me">
-          <input placeholder="name" className="contact-input" id="name" />
-          <input placeholder="email" className="contact-input" id="email" />
-          <input placeholder="message" className="contact-input" id="message" />
-          <button id="submit">Submit</button>
-          <div>
-            <h3 id="response"></h3>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'message') {
+      setMessage(inputValue);
+    } else {
+      setPassword(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    if (!checkPassword(password)) {
+      setErrorMessage(
+        `Choose a more secure password`
+      );
+      return;
+    }
+    alert(`Hello`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setMessage('');
+    setPassword('');
+    setEmail('');
+  };
+
+  return (
+    <div>
+      <h2>Contact Form</h2>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onBlur={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Message"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
