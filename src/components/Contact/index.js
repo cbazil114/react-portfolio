80e40d6d13b0038f1ab672fd6342207617c55f9e
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-03-10 17:29:20 -0500

    Issues with contact - message and incorrecrt email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -19,18 +19,30 @@ export default function Contact() {
 
     // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'message') {
-      setMessage(inputValue);
+      if (validateEmail(inputValue)) {
+        setErrorMessage('');
+        setEmail(inputValue);
+        // Check if the email is valid after input is done (using onBlur to trigger once cursor leaves email input box)
+      } else {
+        setErrorMessage('');
+      setErrorMessage('Please enter a valid email');
+    }} else if (inputType === 'message') {
+      if (!setMessage(inputValue)) {
+        setErrorMessage('Please enter a message')
+      } else {
+        setErrorMessage('');
+        setMessage(inputValue);
+      }
     } else {
-      setPassword(inputValue);
+      if (checkPassword(inputValue)) {
+        setErrorMessage('');
+        setPassword(inputValue);
+      } else {
+        setErrorMessage(
+          `Please enter a password`
+        );
     }
-    if (!validateEmail(email)) {
-      setErrorMessage('Email is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
+  }
   };
   
 
@@ -38,13 +50,7 @@ export default function Contact() {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password`
-      );
-      return;
-    }
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     alert(`Hello`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
@@ -67,14 +73,14 @@ export default function Contact() {
         <input
           defaultValue={message}
           name="message"
-          onChange={handleInputChange}
+          onBlur={handleInputChange}
           type="text"
           placeholder="Message"
         />
         <input
           defaultValue={password}
           name="password"
-          onChange={handleInputChange}
+          onBlur={handleInputChange}
           type="password"
           placeholder="Password"
         />
